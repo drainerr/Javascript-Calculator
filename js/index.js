47a723db5990ce00a7b1edb72857bf8beff95116
Georgeave <67703102+Georgeave@users.noreply.github.com>
2021-06-20 14:11:12 +0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const display = document.getElementById('display');
+const buttons = document.getElementsByClassName('.button');
+
+var getNum = () =>{
+    var key = event.target.innerText;
+    print(key);
+}
+
+var print = (value) =>{
+    
+    if(display.innerHTML == '0'){
+        display.innerHTML = '';
+        if(!isNaN(value) || value == '-')
+        display.innerHTML += value;
+    
+    } else{
+        if(value == 'C'){
+            display.innerHTML = '0';
+        }
+        if(value == '='){
+            display.innerHTML = eval(display.innerHTML);
+        }else{   
+                if(value != 'C'){
+                display.innerHTML += value;
+                check(display.innerHTML);
+                if(display.innerHTML.length > 15){
+                display.innerHTML = 'Digit Limit Met'
+                }
+            }
+        }  
+    }
+}
+
+var check = (val) =>{
+    var lastChar = val[val.length-1];
+    var prevChar = val[val.length-2];
+    if (typeof(lastChar) ==typeof(prevChar) && isNaN(lastChar) && isNaN(prevChar)){
+        display.innerHTML = val.slice(0,-1);
+  }
+}
+
